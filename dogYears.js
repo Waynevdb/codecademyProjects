b6bd77bd806bf71381f09236145a33ca50ff9b82
Wayne Van Der Berg <102765856+Waynevdb@users.noreply.github.com>
2022-05-22 20:51:26 +0200

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,9 @@
+let myAge = 5
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge -2
+laterYears *= 4
+//^ each following year equates to 4 dog years
+let myAgeInDogYears = earlyYears + laterYears
+let myName = 'Wayne'
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
